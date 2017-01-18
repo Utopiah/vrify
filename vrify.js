5f805fad01521b4d2ae0e9859daa9af03993bd83
Fabien Benetou <fabien-services@benetou.fr>
2017-01-18 14:07:18 +0100

    works

---
+++ vrify.js
@@ -0,0 +1,120 @@
+/* 
+ * alternatives
+ *
+ * https://github.com/kfarr/jurassic-file-navigator
+ * https://github.com/jhspetersson/filemanager-vr
+ *
+ */
+
+// removing existing CSS
+var elements = document.querySelectorAll('link[rel=stylesheet]');
+for(var i=0;i<elements.length;i++){
+	    elements[i].parentNode.removeChild(elements[i]);
+}
+
+let x = document.body.querySelectorAll('body > *');
+let index = 0;
+for( index=0; index < x.length; index++ ) {
+	x[index].style.display = "none";
+	//console.log(x[index], 'removed');
+}
+
+// adding aframe and components
+scripts = [
+	"https://aframe.io/releases/0.4.0/aframe.min.js",
+	"https://rawgit.com/bryik/aframe-bmfont-text-component/master/dist/aframe-bmfont-text-component.min.js",
+];
+index = 0;
+for( index=0; index < scripts.length; index++ ) {
+	script = document.createElement("script");
+	script.setAttribute("src", scripts[index]);
+	document.head.appendChild(script);
+}
+
+// making a scene
+scene = document.createElement("a-scene");
+scene.setAttribute("debug", "");
+document.body.appendChild(scene);
+
+// adding a sky, easier to debug
+sky = document.createElement("a-sky");
+sky.setAttribute("color", "lightblue");
+scene.appendChild(sky);
+
+// adding files, probably browser specific but for now focusing on Nightly for link traversal
+function addFiles(){
+	files = document.createElement("a-entity");
+	files.setAttribute("position", "0 0 -5");
+	files.setAttribute("layout", "type:cube");
+	files.setAttribute("id", "files");
+	scene.appendChild(files);
+
+	x = document.body.querySelectorAll('[class=file]');
+	index = 0;
+	for( index=0; index < x.length; index++ ) {
+		box = document.createElement("a-box");
+		box.setAttribute("color", "white");
+		box.setAttribute("depth", "0.3");
+		box.setAttribute("position", ""+index*1.1+" 1 -5");
+		files.appendChild(box);
+		filename = x[index].href.replace(window.location,"");
+		text = document.createElement("a-entity");
+		text.setAttribute("bmfont-text", "text: "+filename);
+		if (index%2==0)
+			text.setAttribute("position", "-0.5 0.7 0.5");
+		else
+			text.setAttribute("position", "-0.5 1 0.5");
+		box.appendChild(text);
+	}
+}
+
+function addFolders(){
+	folders = document.createElement("a-entity");
+	folders.setAttribute("position", "0 0.8 -5");
+	folders.setAttribute("layout", "type:cube");
+	folders.setAttribute("id", "folders");
+	scene.appendChild(folders);
+
+	x = document.body.querySelectorAll('[class=dir]');
+	index = 0;
+	for( index=0; index < x.length; index++ ) {
+		box = document.createElement("a-box");
+		box.setAttribute("color", "brown");
+		box.setAttribute("position", ""+index*1.1+" -1 -5");
+		folders.appendChild(box);
+		filename = x[index].href.replace(window.location,"");
+		text = document.createElement("a-entity");
+		text.setAttribute("bmfont-text", "text: "+filename);
+		if (index%2==0)
+			text.setAttribute("position", "-0.5 -0.7 0.5");
+		else
+			text.setAttribute("position", "-0.5 -1 0.5");
+		box.appendChild(text);
+	}
+}
+
+/*
+ * trying to add the script later i.e. after the scene has loaded but still unable to register
+ */
+function lateScripts(){
+	latescripts = [
+		"https://rawgit.com/ngokevin/kframe/master/components/layout/dist/aframe-layout-component.min.js",
+	];
+	index = 0;
+	for( index=0; index < latescripts.length; index++ ) {
+		script = document.createElement("script");
+		script.setAttribute("src", latescripts[index]);
+		document.head.appendChild(script);
+	}
+	// somehow doesn't work
+}
+
+if (scene.hasLoaded) {
+	run();
+} else {
+	scene.addEventListener('loaded', run);
+}
+function run () {
+	addFiles();
+	addFolders();
+}
